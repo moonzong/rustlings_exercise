1a974b3b29e34cd0aee9a25f174d1a23b2edc006
moonzong <moonzong@outlook.com>
2025-12-17 01:54:08 +0800

    day02:完成基础函数和if语句学习

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -2,11 +2,12 @@
 fn picky_eater(food: &str) -> &str {
     if food == "strawberry" {
         "Yummy!"
-    } else {
-        1
+    } else if food == "potato" {
+        "I guess I can eat that."
+    }else {
+        "No thanks!"
     }
 }
-
 fn main() {
     // You can optionally experiment here.
 }
